26267ca86dcfb65cb644c15f18b0f28636d71e79
shiwani bhardwaj <shiwanibhardwaj111@gmail.com>
2026-09-17 15:10:46 +0530

    Add Pandas data cleaning and analysis practice

--- pandas/dataframe.py
+++ pandas/dataframe.py
@@ -20,9 +20,10 @@ print(df.columns)   #columns name
 print(df.dtypes)    #data types
 print(df.describe()) #Summary statistics (numeric columns)
 print(df.info())     #Full information (very important).
-
-
-
+print(df.index)
+print(df.sample(5))   #it will print some random samples of a dataset.  
+print(df.nunique())
+print(df.to_string())    #it will print all the dataset.
 #arithmetic operations....
 df['values']=df['Age']+df['Score']
 print(df)
@@ -35,3 +36,32 @@ df['info']=df['Age'] < 20
 print(df)
 
 print(df["Age"]<=20)
+
+
+#by list inside list
+info=[['saurabh',28],["shivani",22],['abhishek',26]]
+df=pd.DataFrame(info,columns=["names","ages"])
+print(df)
+
+
+#dataframe by numpy array..
+import numpy as np
+data=np.array([[1,2,3],[4,5,6],[7,8,9]])
+df=pd.DataFrame(data,columns=["A","B","C"])
+print(df)
+
+
+data = pd.read_csv("D:/shivani/data science/pandas/titanic/train.csv")
+#selecting data row wise (loc,iloc)
+print(data.loc[2])
+print(data.loc[5:11])  #start index:end index (inclusive)
+
+print(data.iloc[2])
+print(data.iloc[5:11])  #start index:end index (exclusive)
+
+#cells-row,col
+print(data.loc[2,["Name",'Age']])
+print(data.iloc[2:9,2:5])
+
+print(data.at[2,'Name'])
+print(data.iat[2,3])
